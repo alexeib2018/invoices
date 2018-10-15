5a0e4f117882ea556cce3d7a92b985a95e351ee2
Alexei Bushuev <alexeib.2018@gmail.com>
2018-10-15 19:08:01 +0300

    developed account info

--- cgi/pdf.pl
+++ cgi/pdf.pl
@@ -10,8 +10,16 @@ my %invoice = ("Number" => "SO345372",
                "billToCity" => "San Ysidro CA 92173",
                "shipToName" => "Chevron #5212 McLane Southern California #812131",
                "shipToAddress" => "220 Sycamore Rd",
-               "shipToCity" => "San Ysidro CA 92173"
-               );
+               "shipToCity" => "San Ysidro CA 92173",
+               "terms" => "30D",
+               "shipVia" => "HOUSE",
+               "shipDate" => "2018-10-09",
+               "dueDate" => "2018-10-27",
+               "account" => "C0002931",
+               "poNumber" => "111",
+               "poDate" => "2018-10-09",
+               "salesPerson" => "HOUSE"
+              );
 
 my $pdf = new PDF::Report(PageSize => "letter",
                           PageOrientation => "portrait");
@@ -50,7 +58,7 @@ reset_font();
 
 $pdf->addImgScaled("../img/logo.png", $width - 165 - $margin, $yPos - $lh*2.5, 0.4);
 
-$yPos -= $lh*5;
+$yPos -= $lh * 5;
 
 $pdf->setAlign("Right");
 $pdf->setAddTextPos($width-$margin, $yPos);
@@ -59,7 +67,7 @@ $pdf->setAddTextPos($width-$margin, $yPos - $lh);
 $pdf->addText("Invoice Date: $invoice{'Date'}");
 reset_font();
 
-$yPos -= $lh*2;
+$yPos -= $lh * 2;
 
 $pdf->setAddTextPos($margin, $yPos);
 $pdf->addText("Bill");
@@ -72,11 +80,59 @@ $pdf->addText($invoice{'billToAddress'});
 $pdf->setAddTextPos($margin + 20, $yPos - $lh*3);
 $pdf->addText($invoice{'billToCity'});
 
+$pdf->setAddTextPos($width/2 + $margin, $yPos);
+$pdf->addText("Ship");
+$pdf->setAddTextPos($width/2 + $margin, $yPos - $lh);
+$pdf->addText("To:");
+$pdf->setAddTextPos($width/2 + $margin + 20, $yPos - $lh);
+$pdf->addText($invoice{'shipToName'});
+$pdf->setAddTextPos($width/2 + $margin + 20, $yPos - $lh*2);
+$pdf->addText($invoice{'shipToAddress'});
+$pdf->setAddTextPos($width/2 + $margin + 20, $yPos - $lh*3);
+$pdf->addText($invoice{'shipToCity'});
+
 $yPos -= $lh * 5;
 
 $pdf->setGfxLineWidth(2);
 $pdf->drawLine($margin, $yPos, $width-$margin, $yPos);
 $pdf->setGfxLineWidth(1);
+
+$pdf->setAddTextPos($margin, $yPos - $lh);
+$pdf->addText("Terms");
+$pdf->setAddTextPos($margin, $yPos - $lh*2);
+$pdf->addText("Ship via");
+$pdf->setAddTextPos($margin, $yPos - $lh*3);
+$pdf->addText("Ship Date");
+$pdf->setAddTextPos($margin, $yPos - $lh*4);
+$pdf->addText("Due Date");
+
+$pdf->setAddTextPos($margin + 70, $yPos - $lh);
+$pdf->addText($invoice{'terms'});
+$pdf->setAddTextPos($margin + 70, $yPos - $lh*2);
+$pdf->addText($invoice{'shipVia'});
+$pdf->setAddTextPos($margin + 70, $yPos - $lh*3);
+$pdf->addText($invoice{'shipDate'});
+$pdf->setAddTextPos($margin + 70, $yPos - $lh*4);
+$pdf->addText($invoice{'dueDate'});
+
+$pdf->setAddTextPos($width/2 + $margin, $yPos - $lh);
+$pdf->addText("Account No.");
+$pdf->setAddTextPos($width/2 + $margin, $yPos - $lh*2);
+$pdf->addText("P.O. Number");
+$pdf->setAddTextPos($width/2 + $margin, $yPos - $lh*3);
+$pdf->addText("P.O. Date");
+$pdf->setAddTextPos($width/2 + $margin, $yPos - $lh*4);
+$pdf->addText("Sales Person");
+
+$pdf->setAddTextPos($width/2 + $margin + 70, $yPos - $lh);
+$pdf->addText($invoice{'account'});
+$pdf->setAddTextPos($width/2 + $margin + 70, $yPos - $lh*2);
+$pdf->addText($invoice{'poNumber'});
+$pdf->setAddTextPos($width/2 + $margin + 70, $yPos - $lh*3);
+$pdf->addText($invoice{'poDate'});
+$pdf->setAddTextPos($width/2 + $margin + 70, $yPos - $lh*4);
+$pdf->addText($invoice{'salesPerson'});
+
 
 
 # my @lines = split_text('The Brown Bag Sandwich', 100);
@@ -105,15 +161,15 @@ my $notice2 = "PACA Notice: The perishable agricultural commodities listed on th
 
 my $notice3 = "NOTICE: Past due invoices as well as any judgments arising from the collection of such invoices shall accrue annual interest at the
  rate of 18% or at the maximum legal rate, whichever is lower. Seller shall be entitled to collect reasonable attorney`s fees and
- expenses as part of an action to collect on this invoice. Actual  attorney`s  fees  incurred  in  bringing  any  action  to  collect  on  this  invoice
+ expenses as part of an action to collect on this invoice. Actual attorney`s fees incurred in bringing any action to collect on this invoice
  and/or enforcing any judgment granted and interest shall be considered as additional sums owed in connection with this transaction.";
 
-$pdf->addParagraph($notice1, $margin, $yPos, $width-$margin*2,  80, 25, $lh);
-$yPos -= 80;
-$pdf->addParagraph($notice2, $margin, $yPos, $width-$margin*2,  80, 25, $lh);
-$yPos -= 96;
-$pdf->addParagraph($notice3, $margin, $yPos, $width-$margin*2, 100, 25, $lh);
-$yPos -= 96;
+$pdf->addParagraph($notice1, $margin, $yPos, $width-$margin*2, $lh*4, 25, $lh);
+$yPos -= $lh*4;
+$pdf->addParagraph($notice2, $margin, $yPos, $width-$margin*2, $lh*5, 25, $lh);
+$yPos -= $lh*5;
+$pdf->addParagraph($notice3, $margin, $yPos, $width-$margin*2, $lh*5, 25, $lh);
+$yPos -= $lh*5;
 
 print "Content-Type: application/pdf\n\n";
 print $pdf->Finish(\&footer);
